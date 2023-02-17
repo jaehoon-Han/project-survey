bc1af2da019179a84b3a4fa65d1a14fc1d7ca1d7
SoyiJung-kor <soyi@maumlab.co.kr>
2023-02-17 13:29:54 +0900

    :card_file_box: fix the user module

--- dist/user/user.service.d.ts
+++ dist/user/user.service.d.ts
@@ -1,12 +1,10 @@
-import { SurveyResponseService } from 'src/survey-response/survey-response.service';
 import { Repository } from 'typeorm';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { User } from './entities/user.entity';
 export declare class UserService {
     private userRepository;
-    private surveyResponse;
-    constructor(userRepository: Repository<User>, surveyResponse: SurveyResponseService);
+    constructor(userRepository: Repository<User>);
     create(createUserInput: CreateUserInput): Promise<User>;
     findAll(): Promise<User[]>;
     getUserWithResponse(): Promise<User[]>;
