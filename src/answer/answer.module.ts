2b79c4c1b03f5dfe88487fdffb5554914046437a
SoyiJung-kor <soyi@maumlab.co.kr>
2023-02-17 13:36:40 +0900

    :card_file_box: fix the answer module

--- src/answer/answer.module.ts
+++ src/answer/answer.module.ts
@@ -3,22 +3,9 @@ import { AnswerService } from './answer.service';
 import { AnswerResolver } from './answer.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Answer } from './entities/answer.entity';
-import { SurveyResponse } from 'src/survey-response/entities/survey-response.entity';
-import { QuestionOption } from 'src/question-option/entities/question-option.entity';
-import { QuestionOptionService } from 'src/question-option/question-option.service';
-import { SurveyResponseService } from 'src/survey-response/survey-response.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([QuestionOption]),
-    TypeOrmModule.forFeature([SurveyResponse]),
-    TypeOrmModule.forFeature([Answer]),
-  ],
-  providers: [
-    AnswerResolver,
-    AnswerService,
-    QuestionOptionService,
-    SurveyResponseService,
-  ],
+  imports: [TypeOrmModule.forFeature([Answer])],
+  providers: [AnswerResolver, AnswerService],
 })
 export class AnswerModule {}
